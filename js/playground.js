43e5f4b33708d9714790b1bfeed3ef415de99dd1
abeaty149 <abeaty@bgsu.edu>
2019-03-14 13:33:31 -0400

    updated front page

--- js/playground.js
+++ js/playground.js
@@ -26,9 +26,87 @@ var circle=$('<div class="circle"</div>');
 }
 
 
-    
+  $("#move").click(function move(){
+
+	var stop;
+    if (stop==true){
+    	clearInterval(id);
+    }
+    	else {
+    		stop=true;
+    	$(".circle").each(function(){
+    		var switchx=true;
+    		var switchy=true;
+    		var id = setInterval(frame, 10);
+			function frame() {
+    	var circle1 = this;
+    	var x = $(circle1).position();
+
+        if (switchx=false) {
+        	if(x.left<=0){
+        		switchx=true;
+        		$(circle1).css({
+           	background: getRandomColor(),
+           	border-color: getRandomColor()
+           });
+            }
+            else { $(circle1).css({
+           	left:  x.left-1,
+           });
+        }
+    }
+         
+        else if(switchx=true) {
+        	if(x.left>=750){
+        		switchx=false;
+        		$(circle1).css({
+           	background: getRandomColor(),
+           	border-color: getRandomColor()
+           });
+        	}
+        	else {$(circle1).css({
+           	left: x.left+1,
+           });
+        }
+    }
+    if (switchy=false) {
+        	if(x.top<=0){
+        		switchy=true;
+        		$(circle1).css({
+           background: getRandomColor(),
+           border-color: getRandomColor()
+           });
+            }
+            else { $(circle1).css({
+           	top:  x.top+1,
+           });
+        }
+    }
+         
+        else if(switchy=true) {
+        	if(x.top>=750){
+        		switchy=false;
+        		$(circle1).css({
+           background: getRandomColor(),
+           border-color: getRandomColor()
+           });
+        	}
+        	else {$(circle1).css({
+           	top: x.top-1,
+           });
+        }
+    }
+}
 
 
+       });//each
+    }
+    });//move
+
+
+
+
+  
 $("#add").click(function(e)
 {
 	addCircle();
